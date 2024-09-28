b8b41240c54d3ffb670e8242c15de0e711d6ba6b
Abigail Chavez <chavezlopezabigail28@gmail.com>
2024-09-28 12:24:39 -0600

    Proyecto con React

---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+// src/components/NavBar.js
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+const NavBar = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        {/* UXsperience a la izquierda */}
+        <Navbar.Brand href="#home">UXsperience</Navbar.Brand>
+        
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          {/* Links a la derecha */}
+          <Nav className="ms-auto">
+            <Nav.Link href="#perfil">Perfil</Nav.Link>
+            <Nav.Link href="#historial">Historial</Nav.Link>
+            <Nav.Link href="#evaluando">Evaluando</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
