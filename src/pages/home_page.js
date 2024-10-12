69048569865fbde17c4a973a590b2604622d18dd
DonCarlosD <l20121019@morelia.tecnm.mx>
2024-10-12 16:35:26 -0600

    pantalla home page

--- src/pages/home_page.js
+++ src/pages/home_page.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import NavBarLog from '../components/NavBarLog';
+import Card from '../components/cards';
 
 function HomePage() {
   return (
-    <div>
-        <NavBarLog />
-      <h1>HomePage</h1>
+    <div className="bg-fondo h-screen overflow-hidden ">
+    <NavBarLog className="fixed top-0 left-0 w-full z-10" />
+    <div className='mt-16 pt-16'>
+      <div className='flex justify-center gap-6 mt-9'>
+      <Card title="Nueva Rubrica" icono="nueva"/>
+      <Card title="Evaluar"  icono="calificar" />
+      <Card title="Ver rubricas"       icono="rubricas" />
+      </div>
     </div>
+  </div>
+
   );
 }
 
