a648e85323aed4ba70e6ec7b721dc54eb645f704
DonCarlosD <l20121019@morelia.tecnm.mx>
2024-10-13 00:19:13 -0600

    menu sidebar terminado

--- src/pages/home_page.js
+++ src/pages/home_page.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import NavBarLog from '../components/NavBarLog';
+import Navbar from '../components/NavBarLog';
 import Card from '../components/cards';
+
 
 function HomePage() {
   return (
     <div className="bg-fondo h-screen overflow-hidden ">
-    <NavBarLog className="fixed top-0 left-0 w-full z-10" />
+   
+    <Navbar className="pl-5" />
     <div className='mt-16 pt-16'>
       <div className='flex justify-center gap-6 mt-9'>
       <Card title="Nueva Rubrica" icono="nueva"/>
