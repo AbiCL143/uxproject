44d384b8149cd6199fb764057c8c5a5d38732593
Luis Martin <luismartin15lm@outlook.com>
2024-10-15 00:06:49 -0600

    Fokin menu no queda

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import NuevaRubrica from './pages/nuevaRubrica';
 import Resumen_de_Rubrica from './pages/resumen_de_Rubrica';
 import Software from './pages/softwares';
 import Prueba from './pages/prueba';
+import NewRubric from './pages/newRubric';
 
 function App() {
   return (
@@ -28,7 +29,7 @@ function App() {
       <Route path='/evaluacion_de_Software' element={<Evaluacion_de_Software/>}/>
       <Route path='/nuevaRubrica' element={<NuevaRubrica/>}/>
       <Route path='/resumen_de_rubrica' element={<Resumen_de_Rubrica/>}/>
-
+      <Route path='/newRubric' element={<NewRubric/>}/>
       </Routes>
     </Router>
   );
--- src/pages/resumen_de_Rubrica.js
+++ src/pages/resumen_de_Rubrica.js
@@ -2,12 +2,19 @@ import React, { useState, useEffect } from 'react';
 import NavBarLog from '../components/NavBarLog';
 import RúbricaPDF from '../components/descargar'; // Asegúrate de ajustar la ruta
 import { Link } from 'react-router-dom';
+
 function Resumen_de_Rubrica() {
-    const [openIndex, setOpenIndex] = useState(null);
+    const [openIndex, setOpenIndex] = useState(null); // Para manejar el acordeón
+    const [isModalOpen, setIsModalOpen] = useState(false); // Para manejar la apertura y cierre del modal
+    const [selectedCategory, setSelectedCategory] = useState(''); // Para manejar la categoría seleccionada
+    const [categories, setCategories] = useState([]); // Para manejar las categorías
+
+    // Datos de ejemplo para la rúbrica
     const data = {
         "nombre_rubrica": "Evaluación de Usabilidad y Accesibilidad",
         "categorias": [
             {
+                "id": 1,
                 "nombre": "Usabilidad",
                 "criterios": [
                     {
@@ -23,24 +30,11 @@ function Resumen_de_Rubrica() {
                             "¿Los elementos de la interfaz son claros y comprensibles?",
                             "¿El texto es legible y está bien presentado?"
                         ]
-                    },
-                    {
-                        "nombre": "Consistencia de los elementos",
-                        "preguntas": [
-                            "¿Los elementos de la interfaz mantienen un estilo y comportamiento uniforme?",
-                            "¿Se aplican los mismos patrones en toda la interfaz?"
-                        ]
-                    },
-                    {
-                        "nombre": "Eficiencia en el uso",
-                        "preguntas": [
-                            "¿El usuario puede completar tareas de manera eficiente?",
-                            "¿Se minimiza el número de clics necesarios para realizar acciones?"
-                        ]
                     }
                 ]
             },
             {
+                "id": 2,
                 "nombre": "Accesibilidad",
                 "criterios": [
                     {
@@ -49,131 +43,17 @@ function Resumen_de_Rubrica() {
                             "¿La interfaz es compatible con tecnologías de asistencia?",
                             "¿Se proporciona información suficiente para que los usuarios comprendan el contenido?"
                         ]
-                    },
-                    {
-                        "nombre": "Uso de contrastes adecuados",
-                        "preguntas": [
-                            "¿Se utilizan contrastes de color que facilitan la lectura?",
-                            "¿Los colores utilizados son accesibles para personas con daltonismo?"
-                        ]
-                    },
-                    {
-                        "nombre": "Textos alternativos para imágenes",
-                        "preguntas": [
-                            "¿Se proporcionan descripciones de texto alternativo para imágenes?",
-                            "¿Las descripciones son suficientemente detalladas para comprender el contenido?"
-                        ]
-                    },
-                    {
-                        "nombre": "Facilidad de navegación para usuarios con discapacidad",
-                        "preguntas": [
-                            "¿La navegación es accesible para usuarios con diferentes discapacidades?",
-                            "¿Se pueden utilizar atajos de teclado para navegar por la interfaz?"
-                        ]
-                    }
-                ]
-            },
-            {
-                "nombre": "Simplicidad",
-                "criterios": [
-                    {
-                        "nombre": "Diseño limpio y minimalista",
-                        "preguntas": [
-                            "¿La interfaz tiene un diseño que evita la sobrecarga visual?",
-                            "¿Se utilizan solo los elementos necesarios para la función principal?"
-                        ]
-                    },
-                    {
-                        "nombre": "Eliminación de elementos innecesarios",
-                        "preguntas": [
-                            "¿Se han eliminado elementos que no aportan valor al usuario?",
-                            "¿Cada elemento en la interfaz tiene un propósito claro?"
-                        ]
-                    },
-                    {
-                        "nombre": "Acceso directo a funciones principales",
-                        "preguntas": [
-                            "¿Las funciones más importantes son fácilmente accesibles?",
-                            "¿Se pueden encontrar rápidamente las acciones más utilizadas?"
-                        ]
-                    },
-                    {
-                        "nombre": "Lenguaje claro y comprensible",
-                        "preguntas": [
-                            "¿El lenguaje utilizado es fácil de entender para el usuario?",
-                            "¿Se evitan tecnicismos innecesarios en la redacción?"
-                        ]
-                    }
-                ]
-            },
-            {
-                "nombre": "Centrada en el Usuario",
-                "criterios": [
-                    {
-                        "nombre": "Investigación de usuarios previa al diseño",
-                        "preguntas": [
-                            "¿Se realizó una investigación sobre las necesidades del usuario antes del diseño?",
-                            "¿Se llevaron a cabo entrevistas o encuestas con los usuarios potenciales?"
-                        ]
-                    },
-                    {
-                        "nombre": "Incorporación de feedback de usuarios",
-                        "preguntas": [
-                            "¿Se ha considerado el feedback de los usuarios en el diseño?",
-                            "¿Se han realizado pruebas de usabilidad para recopilar opiniones?"
-                        ]
-                    },
-                    {
-                        "nombre": "Adaptabilidad a diferentes perfiles de usuario",
-                        "preguntas": [
-                            "¿La interfaz se adapta a las necesidades de diferentes tipos de usuarios?",
-                            "¿Se ofrece personalización para diferentes niveles de experiencia?"
-                        ]
-                    },
-                    {
-                        "nombre": "Enfoque en necesidades del usuario",
-                        "preguntas": [
-                            "¿La interfaz aborda directamente las necesidades de los usuarios?",
-                            "¿Se han considerado diferentes escenarios de uso al diseñar la interfaz?"
-                        ]
-                    }
-                ]
-            },
-            {
-                "nombre": "Consistencia",
-                "criterios": [
-                    {
-                        "nombre": "Uso uniforme de colores y tipografías",
-                        "preguntas": [
-                            "¿Se utilizan colores y tipografías de manera uniforme en toda la interfaz?",
-                            "¿Los estilos se mantienen consistentes en todas las páginas y secciones?"
-                        ]
-                    },
-                    {
-                        "nombre": "Estándares de diseño aplicados en toda la interfaz",
-                        "preguntas": [
-                            "¿Se han aplicado estándares de diseño reconocidos?",
-                            "¿Los elementos de la interfaz cumplen con las pautas de accesibilidad?"
-                        ]
-                    },
-                    {
-                        "nombre": "Comportamiento predecible de los elementos",
-                        "preguntas": [
-                            "¿Los elementos de la interfaz se comportan de manera predecible?",
-                            "¿Los usuarios pueden anticipar la acción de los elementos?"
-                        ]
-                    },
-                    {
-                        "nombre": "Uso de patrones de diseño conocidos",
-                        "preguntas": [
-                            "¿Se utilizan patrones de diseño que los usuarios ya conocen?",
-                            "¿Los usuarios pueden reconocer la funcionalidad basada en experiencias previas?"
-                        ]
                     }
                 ]
             }
         ]
-    }
+    };
+
+    useEffect(() => {
+        // Cargamos las categorías desde los datos de ejemplo al estado
+        setCategories(data.categorias);
+    }, [data]);
+
     const toggleAccordion = (index) => {
         setOpenIndex(openIndex === index ? null : index);
     };
@@ -183,9 +63,8 @@ function Resumen_de_Rubrica() {
     };
 
     const handleConfirm = () => {
-        // Aquí puedes manejar la lógica después de confirmar, como enviar la categoría seleccionada a tu API
         console.log('Categoría seleccionada:', selectedCategory);
-        toggleModal(); // Cerrar el modal después de confirmar
+        toggleModal(); // Cierra el modal después de confirmar
     };
 
     return (
@@ -232,71 +111,6 @@ function Resumen_de_Rubrica() {
                     </div>
 
                     {/* Repite el mismo patrón para los otros acordeones */}
-                    {/* Acordeón 2 */}
-                    <h2 id="accordion-collapse-heading-2">
-                        <div className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-200 bg-[#333537] border border-b-0 border-gray-700 focus:ring-4 focus:ring-gray-700 dark:focus:ring-gray-800 dark:border-gray-600 hover:bg-[#1E1F20] gap-3">
-                            <button 
-                                type="button" 
-                                className="flex-1 text-left"
-                                onClick={() => toggleAccordion(2)} 
-                                aria-expanded={openIndex === 2} 
-                                aria-controls="accordion-collapse-body-2"
-                            >
-                                <span className="text-[#3777C3]">Is there a Figma file available?</span>
-                            </button>
-                            <svg data-accordion-icon className={`w-3 h-3 rotate-180 shrink-0 text-[#C6C9C7] ${openIndex === 2 ? '' : 'rotate-0'}`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5"/>
-                            </svg>
-                        </div>
-                    </h2>
-                    <div id="accordion-collapse-body-2" className={`transition-all duration-300 ease-in-out ${openIndex === 2 ? 'block' : 'hidden'}`} aria-labelledby="accordion-collapse-heading-2">
-                        <div className="p-5 border border-b-0 border-gray-700 dark:border-gray-600 bg-[#1E1F20]">
-                            <p className="mb-2 text-gray-300">Flowbite is first conceptualized and designed using the Figma software so everything you see in the library has a design equivalent in our Figma file.</p>
-                            <p className="text-gray-300">Check out the <a href="https://flowbite.com/figma/" className="text-[#5082ED] hover:underline">Figma design system</a> based on the utility classes from Tailwind CSS and components from Flowbite.</p>
-
-                            {/* Botón de Mover que abre el modal */}
-                            <button 
-                                type="button" 
-                                className="mt-4 text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-                                onClick={toggleModal}
-                            >
-                                Mover
-                            </button>
-                        </div>
-                    </div>
-
-                    {/* Acordeón 3 */}
-                    <h2 id="accordion-collapse-heading-3">
-                        <div className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-200 bg-[#333537] border border-gray-700 focus:ring-4 focus:ring-gray-700 dark:focus:ring-gray-800 dark:border-gray-600 hover:bg-[#1E1F20] gap-3">
-                            <button 
-                                type="button" 
-                                className="flex-1 text-left"
-                                onClick={() => toggleAccordion(3)} 
-                                aria-expanded={openIndex === 3} 
-                                aria-controls="accordion-collapse-body-3"
-                            >
-                                <span className="text-[#3777C3]">What are the differences between Flowbite and Tailwind UI?</span>
-                            </button>
-                            <svg data-accordion-icon className={`w-3 h-3 rotate-180 shrink-0 text-[#C6C9C7] ${openIndex === 3 ? '' : 'rotate-0'}`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5"/>
-                            </svg>
-                        </div>
-                    </h2>
-                    <div id="accordion-collapse-body-3" className={`transition-all duration-300 ease-in-out ${openIndex === 3 ? 'block' : 'hidden'}`} aria-labelledby="accordion-collapse-heading-3">
-                        <div className="p-5 border border-b-0 border-gray-700 dark:border-gray-600 bg-[#1E1F20]">
-                            <p className="mb-2 text-gray-300">Flowbite is an open-source library of interactive components built on top of Tailwind CSS.</p>
-                            <p className="text-gray-300">While Tailwind UI is a premium component library with pre-designed components.</p>
-
-                            {/* Botón de Mover que abre el modal */}
-                            <button 
-                                type="button" 
-                                className="mt-4 text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-                                onClick={toggleModal}
-                            >
-                                Mover
-                            </button>
-                        </div>
-                    </div>
                 </div>
 
                 {/* Botón de Descargar fuera del acordeón */}
@@ -306,9 +120,8 @@ function Resumen_de_Rubrica() {
                         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                         onClick={() => console.log('Descargando...')}
                     >
-                        <RúbricaPDF  data={data}/>
-                        <Link to="/home" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">  Terminar </Link>
-
+                        <RúbricaPDF data={data}/>
+                        <Link to="/home" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Terminar</Link>
                     </div>
                 </div>
 
@@ -329,7 +142,7 @@ function Resumen_de_Rubrica() {
                             >
                                 <option value="" disabled>Seleccione una categoría</option>
                                 {categories.map((category) => (
-                                    <option key={category.id} value={category.id}>{category.name}</option>
+                                    <option key={category.id} value={category.id}>{category.nombre}</option>
                                 ))}
                             </select>
 
